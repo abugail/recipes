26ef21da19d5c4b295f410376eff058088b6afab
Chris Morris <morriswchris@gmail.com>
2016-10-10 22:35:24 -0400

    trying again

--- bin/index.js
+++ bin/index.js
@@ -12,11 +12,12 @@ glob(`${__base}/!(node_modules|jekyll)**/*.md`, (er, files) => {
     let file_name = path.basename(file);
     let file_data = fs.readFileSync(file, {encoding: "utf-8"});
     let stats = fs.statSync(file);
+    let file_date = new Date(stats.mtime);
     let jekyll_file_data = jekyll(file_data, {
-      date: new Date(stats.mtime),
+      date: file_date,
       author: "Abigail Morris"
     });
 
-    fs.writeFileSync(`${__base}/jekyll/_posts/${file_name}`, jekyll_file_data);
+    fs.writeFileSync(`${__base}/jekyll/_posts/${file_date.getFullYear()}-${file_date.getMonth() + 1}-${file_date.getDate()}-${file_name}`, jekyll_file_data);
   }
 });
