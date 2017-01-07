9950650f7fe45b961b753f32e61624fbe6c4f767
Abi Morris <hartwick.abigail@gmail.com>
2017-01-07 01:45:10 +0000

    Updates

--- bin/index.js
+++ bin/index.js
@@ -2,24 +2,20 @@ const fs     = require("fs");
 const jekyll = require("jekyll-meta-from-markdown");
 const glob = require("glob");
 const path = require("path");
-const del = require("del");
 
 global.__base = "";
-del(`${__base}/jekyll/_posts/*.md`)
-.then(paths => {
-  glob(`${__base}/!(node_modules|jekyll)**/*.md`, (er, files) => {
-    // console.log(files);
-    for (let item in files) {
-      let file = files[item];
-      let file_name = path.basename(file);
-      let file_data = fs.readFileSync(file, {encoding: "utf-8"});
-      let stats = fs.statSync(file);
-      let file_date = new Date(stats.mtime);
-      let jekyll_file_data = jekyll(file_data, {
-        date: file_date,
-        author: "Abigail Morris"
-      });
-      fs.writeFileSync(`${__base}/jekyll/_posts/${file_date.getFullYear()}-${file_date.getMonth() + 1}-${file_date.getDate()}-${file_name}`, jekyll_file_data);
-    }
-  });
+glob(`${__base}/!(node_modules|jekyll)**/*.md`, (er, files) => {
+  // console.log(files);
+  for (let item in files) {
+    let file = files[item];
+    let file_name = path.basename(file);
+    let file_data = fs.readFileSync(file, {encoding: "utf-8"});
+    let stats = fs.statSync(file);
+    let file_date = new Date(stats.mtime);
+    let jekyll_file_data = jekyll(file_data, {
+      date: file_date,
+      author: "Abigail Morris"
+    });
+    fs.writeFileSync(`${__base}/jekyll/_posts/${file_date.getFullYear()}-${file_date.getMonth() + 1}-${file_date.getDate()}-${file_name}`, jekyll_file_data);
+  }
 });
