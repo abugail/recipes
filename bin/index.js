08ad536ccf6b0b70552ce3d94ad0e51e63630cfb
Chris Morris <morriswchris@gmail.com>
2018-08-21 20:04:09 -0400

    fix index.js

--- bin/index.js
+++ bin/index.js
@@ -5,14 +5,14 @@ const path = require("path");
 global.__base = "./";
 glob(`${__base}/!(node_modules|bin)**/*.md`, (er, files) => {
   // console.log(files);
-  let links = {};
+  let links = [];
   for (let item in files) {
     let file = files[item];
     let file_name = path.basename(file).replace('.md', '.html');
     let name = path.basename(file).replace('.md', '').replace('-', ' ');
     let dir_name = path.dirname(file).replace('./', '');
     let file_data = fs.readFileSync(file, {encoding: "utf-8"});
-    links[dir_name](`* [${name}](http://abugail.github.io/recipes/${dir_name}/${file_name})`);
+    links.push(`* [${name}](http://abugail.github.io/recipes/${dir_name}/${file_name})`);
   }
   let link_txt = links.join('\n');
   let text = `# Recipes \n> All the Things I like to cook \n\n${link_txt}`;
